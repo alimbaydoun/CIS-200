ac38ff37a8249ef6160e7c8690fee9d2a91db609
Ali Baydoun <alimbaydoun@gmail.com>
2020-02-06 14:37:02 -0500

    Update

--- Lab 3/Lab 3/Student.h
+++ Lab 3/Lab 3/Student.h
@@ -9,10 +9,15 @@ class Student
 private:
 	string major;
 	int credits;
-
 public:
-	string getMajor;
-	int getCredits;
-
-
+	Student(string m = "General Studies", int c = 0);
+	Student(Student& s);
+	~Student();
+	const string getMajor();
+	const int getCredits();
+	const void getStatus(string& m, int& c);
+	void setMajor(string m);
+	void setCredits(int c);
+	void setStatus(string m, int c);
+	string displayStudent();
 };
