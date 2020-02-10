de0a6a9d1822cf2a839f1a4be2e58da3bcba54a9
Ali Baydoun <alimbaydoun@gmail.com>
2020-02-10 18:29:23 -0500

    update

--- Lab 3/Lab 3/Student.cpp
+++ Lab 3/Lab 3/Student.cpp
@@ -6,7 +6,39 @@
 
 using namespace std;
 
+Student::Student(string m, int c) {
+    this->major = m;
+    this->credits = c;
+    cout << "Constructor has been called! \n";
+}
 
+Student::Student(Student& s) {
+    this->major = s.major;
+    this->credits = s.credits;
+}
+
+Student::~Student() {
+    cout << "Destructor called for " << this->major << " major! \n";
+    this->major = "None";
+    this->credits = -1;
+}
+
+const string Student::getMajor() {
+    return this->major;
+}
+
+const int Student::getCredits() {
+    return this->credits;
+}
+
+const void Student::getStatus(string& m, int& c) {
+    m = this->major;
+    c = this->credits;
+}
+
+void Student::setMajor(string m) {
+    this->major = m;
+}
 
 int main()
 {
