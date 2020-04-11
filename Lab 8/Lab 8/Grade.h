30affd390c57ea0d0e65aed0a8d6b2b484701fe0
Ali Baydoun <alimbaydoun@gmail.com>
2020-04-10 22:29:53 -0400

    update

---
+++ Lab 8/Lab 8/Grade.h
@@ -0,0 +1,27 @@
+#pragma once
+#include<iostream>
+#include<string>
+#include<fstream>
+
+using namespace std;
+
+class Grader
+{
+private:
+	ifstream input;
+	ofstream output;
+
+public:
+	void setOutputFile(string, int);
+	void closeOutputFile();
+	void searchID(string ID);
+	void readFile(string inFile);
+	void calcualteDistribution();
+
+protected:
+	double calculateMidterms(double ex1, double ex2);
+	double calculateFinal(double ex3);
+	double calculateProgs(double p[]);
+	double calculateLabs(double p[]);
+	string calcualteGrade(string s);
+};
